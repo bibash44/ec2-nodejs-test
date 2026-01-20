2bf641f1b52b17c0c7dba0230894af6a3cd77bac
bibash kattel <bibashkatel4@gmail.com>
2026-01-20 12:05:20 +0000

    adds morgan log

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(morgan("combined"))
+app.use(morgan("dev"))
 app.use(
   cors({
     credentials: true,
