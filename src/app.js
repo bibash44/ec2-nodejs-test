e6e2763cae4b9b9c714a03af69e60d993c60f54b
bibash kattel <bibashkatel4@gmail.com>
2026-01-20 11:04:55 +0000

    adds morgan for loggin the request

--- src/app.js
+++ src/app.js
@@ -3,10 +3,12 @@ const port = 8080;
 import express from "express";
 import cors from "cors";
 import routes from "./routes/index.js";
+import morgan  from "morgan";
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(morgan("combined"))
 app.use(
   cors({
     credentials: true,
